a6543541d268296672c22527e1c1e5d0954b1eaf
Mauricio <mauricitogarron@gmail.com>
2026-03-07 02:22:24 -0400

    feat: funcionalidad de descuento por monto

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,4 +6,14 @@ export function calcularPrecioBase(cantidad, precio) {
   return cantidad * precio;
 }
 
+export function calcularDescuentoPorMonto(total) {
+  if (total >= 3000) {
+    return total * 0.05;
+  }
+  if (total >= 1000) {
+    return total * 0.03;
+  }
+  return 0;
+}
+
 export default mostrarCantidad;
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,5 +1,6 @@
 import mostrarCantidad from "./calculadora.js";
 import { calcularPrecioBase } from "./calculadora.js";
+import { calcularDescuentoPorMonto } from "./calculadora.js";
 
 describe("Mostrar cantidad de items", () => {
   it("debería devolver la cantidad ingresada", () => {
@@ -19,4 +20,16 @@ describe("Calcular precio base", () => {
   it("debería calcular correctamente otro caso", () => {
     expect(calcularPrecioBase(3, 20)).toEqual(60);
   });
+});
+
+describe("Descuento por monto de compra", () => {
+
+  it("debería aplicar 3% de descuento para orden mayor o igual a 1000", () => {
+    expect(calcularDescuentoPorMonto(1000)).toEqual(30);
+  });
+
+  it("debería aplicar 5% de descuento para orden mayor o igual a 3000", () => {
+    expect(calcularDescuentoPorMonto(3000)).toEqual(150);
+  });
+
 });
