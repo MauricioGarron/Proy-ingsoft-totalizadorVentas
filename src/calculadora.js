db6a9325bfdefc50a3ac9318fa8b0be79864797c
Mauricio <mauricitogarron@gmail.com>
2026-03-07 02:09:51 -0400

    fix: error de carpetas

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,4 +2,8 @@ function mostrarCantidad(cantidad) {
   return cantidad;
 }
 
+export function calcularPrecioBase(cantidad, precio) {
+  return cantidad * precio;
+}
+
 export default mostrarCantidad;
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,4 +1,5 @@
 import mostrarCantidad from "./calculadora.js";
+import { calcularPrecioBase } from "./calculadora.js";
 
 describe("Mostrar cantidad de items", () => {
   it("debería devolver la cantidad ingresada", () => {
@@ -7,5 +8,15 @@ describe("Mostrar cantidad de items", () => {
 
   it("debería devolver otra cantidad ingresada", () => {
     expect(mostrarCantidad(5)).toEqual(5);
+  });
+});
+
+describe("Calcular precio base", () => {
+  it("debería calcular el precio base multiplicando cantidad por precio", () => {
+    expect(calcularPrecioBase(5, 10)).toEqual(50);
+  });
+
+  it("debería calcular correctamente otro caso", () => {
+    expect(calcularPrecioBase(3, 20)).toEqual(60);
   });
 });
